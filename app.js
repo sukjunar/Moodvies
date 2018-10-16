0375fabc9ae8fd4d26480824bc49a44bc76c0094
Jessica Mccormack <jesslynn.mack@gmail.com>
2018-10-16 10:28:00 -0400

    added pie chart feature and button custimization

--- app.js
+++ app.js
@@ -2,69 +2,137 @@
 //Call Cloud Api
 var myUploadWidget;
 document.getElementById("upload_widget_opener").addEventListener("click", function () {
-  myUploadWidget = cloudinary.openUploadWidget({
-    cloudName: 'dxxy1fbq1', uploadPreset: 'default-preset'
-  }, (error, result) => {
-    const uploadedImage = (result.info.url);
-//Call FaceApi
-    const faceAPI = function (uploadedImage) {
-      const imageURL = uploadedImage
-      console.log(imageURL);
-      const queryURL = "https://api-us.faceplusplus.com/facepp/v3/detect?api_key=Ev9zIZLIwjiT5zSHtXHBYRJTZaaEcHpL&api_secret=lfu6EgDOwcUhGoeM0uVmMi0Io_qM2re_&image_url=" + imageURL + "&return_attributes=emotion";
-      console.log(queryURL);
-      $.ajax({
-        url: queryURL,
-        method: 'POST'
-      }).then(function (response) {
-        const anger = (response.faces[0].attributes.emotion.anger);
-        const disgust = (response.faces[0].attributes.emotion.disgust);
-        const fear = (response.faces[0].attributes.emotion.fear);
-        const happiness = (response.faces[0].attributes.emotion.happiness);
-        const neutral = (response.faces[0].attributes.emotion.neutral);
-        const sadness = (response.faces[0].attributes.emotion.sadness);
-        const surprise = (response.faces[0].attributes.emotion.surprise);
+    myUploadWidget = cloudinary.openUploadWidget({
+        cloudName: 'dxxy1fbq1', uploadPreset: 'default-preset',
+        sources: [
+            "url",
+            "camera",
+            "facebook",
+            "instagram",
+            "local"
+        ],
+        showAdvancedOptions: false,
+        cropping: true,
+        multiple: false,
+        defaultSource: "local",
+        showCompletedButton: true,
+        styles: {
+            palette: {
+                window: "#ffffff",
+                sourceBg: "#f4f4f5",
+                windowBorder: "#90a0b3",
+                tabIcon: "#000000",
+                inactiveTabIcon: "#555a5f",
+                menuIcons: "#555a5f",
+                link: "#0433ff",
+                action: "#339933",
+                inProgress: "#0433ff",
+                complete: "#339933",
+                error: "#B56363",
+                textDark: "#000000",
+                textLight: "#fcfffd"
+            },
+            fonts: {
+                default: null,
+                "sans-serif": {
+                    url: null,
+                    active: true
+                }
+            }
+        }
+    }, (error, result) => {
+        const uploadedImage = (result.info.url);
+        
+    
+        
+        
+        //Call FaceApi
+        const faceAPI = function (uploadedImage) {
+            const imageURL = uploadedImage
+            console.log(imageURL);
+            const queryURL = "https://api-us.faceplusplus.com/facepp/v3/detect?api_key=Ev9zIZLIwjiT5zSHtXHBYRJTZaaEcHpL&api_secret=lfu6EgDOwcUhGoeM0uVmMi0Io_qM2re_&image_url=" + imageURL + "&return_attributes=emotion";
+            console.log(queryURL);
+            $.ajax({
+                url: queryURL,
+                method: 'POST'
+            }).then(function (response) {
+                const anger = (response.faces[0].attributes.emotion.anger);
+                const disgust = (response.faces[0].attributes.emotion.disgust);
+                const fear = (response.faces[0].attributes.emotion.fear);
+                const happiness = (response.faces[0].attributes.emotion.happiness);
+                const neutral = (response.faces[0].attributes.emotion.neutral);
+                const sadness = (response.faces[0].attributes.emotion.sadness);
+                const surprise = (response.faces[0].attributes.emotion.surprise);
 
-        const emotionArray = [anger, disgust,fear,happiness,neutral,sadness,surprise];
-        let max = (Math.max(...emotionArray));
-        const emotionId = emotionArray.indexOf(max);
-        let movieId = "";
-        if (emotionId === 0){
-            $('#info').append(`<p>Angry: ${anger}</p>`);
-            let movieId = 28;
-            displayInfo(movieId);
-        }else if (emotionId === 1){
-            $('#info').append(`<p>Disgusted: ${disgust}<p>`);
-            let movieId = 10749;
-            displayInfo(movieId);
-        }else if (emotionId === 2){
-            $('#info').append(`<p>Scared: ${fear}</p>`);
-            let movieId = 99;
-            displayInfo(movieId);
-        }else if (emotionId === 3){
-            $('#info').append(`<p>Happy: ${happiness}</p>`);
-            let movieId = 53;
-            displayInfo(movieId);
-        }else if (emotionId === 4){
-            $('#info').append(`<p>Neutral: ${neutral}</p>`);
-            let movieId = 14;
-            displayInfo(movieId);
-        }else if (emotionId === 5){ 
-            $('#info').append(`<p>Sad: ${sadness}</p>`);
-            let movieId = 35;
-            displayInfo(movieId);
-        }else{
-            $('#info').append(`<p>Surprised: ${surprise}</p>`);
-            let movieId = 36;
-            displayInfo(movieId);
-        } 
-      })
-    };
-    faceAPI(uploadedImage);
-  }), false})
+
+                var chart = new CanvasJS.Chart("chartContainer", {
+                    animationEnabled: true,
+                    title: {
+                        text: "Emotion Results"
+                    },
+                    data: [{
+                        type: "pie",
+                        startAngle: 240,
+                        yValueFormatString: "##0.00\"%\"",
+                        indexLabel: "{label} {y}",
+                        dataPoints: [
+                            { y: anger, label: "Angry" },
+                            { y: disgust, label: "Disgusted" },
+                            { y: fear, label: "Scared" },
+                            { y: happiness, label: "Happy" },
+                            { y: neutral, label: "Neutral" },
+                            { y: sadness, label: "Sad" },
+                            { y: surprise, label: "Surprised" },
+
+                        ]
+                    }]
+                });
+                chart.render();
+
+
+
+                const emotionArray = [anger, disgust, fear, happiness, neutral, sadness, surprise];
+                let max = (Math.max(...emotionArray));
+                const emotionId = emotionArray.indexOf(max);
+                let movieId = "";
+                if (emotionId === 0) {
+                    $('#info').append(`<p>Angry: ${anger}</p>`);
+                    let movieId = 28;
+                    displayInfo(movieId);
+                } else if (emotionId === 1) {
+                    $('#info').append(`<p>Disgusted: ${disgust}<p>`);
+                    let movieId = 10749;
+                    displayInfo(movieId);
+                } else if (emotionId === 2) {
+                    $('#info').append(`<p>Scared: ${fear}</p>`);
+                    let movieId = 99;
+                    displayInfo(movieId);
+                } else if (emotionId === 3) {
+                    $('#info').append(`<p>Happy: ${happiness}</p>`);
+                    let movieId = 53;
+                    displayInfo(movieId);
+                } else if (emotionId === 4) {
+                    $('#info').append(`<p>Neutral: ${neutral}</p>`);
+                    let movieId = 14;
+                    displayInfo(movieId);
+                } else if (emotionId === 5) {
+                    $('#info').append(`<p>Sad: ${sadness}</p>`);
+                    let movieId = 35;
+                    displayInfo(movieId);
+                } else {
+                    $('#info').append(`<p>Surprised: ${surprise}</p>`);
+                    let movieId = 36;
+                    displayInfo(movieId);
+                }
+            })
+        };
+        faceAPI(uploadedImage);
+    }), false
+})
 //Call Movie API 
 const displayInfo = function (movieId) {
     const genreID = movieId
-    const queryURL = `https://api.themoviedb.org/3/discover/movie?api_key=891a6c3da20369ff339fbce34d72464b&with_genres=${genreID}`
+    const queryURL = `https://api.themoviedb.org/3/discover/movie?api_key=891a6c3da20369ff339fbce34d72464b&language=en-US&sort_by=vote_count.desc&include_adult=false&include_video=true&page=1&with_genres=${genreID}`
     $.ajax({
         url: queryURL,
         method: 'GET'
