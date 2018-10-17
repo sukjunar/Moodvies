dfc39b720bc9a769322cb185ddb772fa92391e95
Jessica Mccormack <jesslynn.mack@gmail.com>
2018-10-17 13:45:44 -0400

    added clear

--- app.js
+++ app.js
@@ -111,6 +111,7 @@ const displayInfo = function (movieId) {
         $('#mainMovie').html(`<img src="http://image.tmdb.org/t/p/w500${response.results[0].poster_path}" style="width: 100%; height: 100%;" alt="${response.results[0].title}">`);
         $('#mainMovieInfo').html("");
         $('#mainMovieInfo').html(`<h1>${response.results[0].title}</h1><p>Rating: ${response.results[0].vote_average}</p><h3>Release Date: ${response.results[0].release_date}</h3><p>Summary: ${response.results[0].overview}</p>`);
+        $('#movieList').html("");
         for (let i = 1; i < 10; i++) {
             $('#movieList').append(`<div class="row"><div class="col-6 poster d-flex flex-row-reverse"><img src="http://image.tmdb.org/t/p/w342${response.results[i].poster_path}" style="width: 50%; height: 100%;"></div><div class="col-3"><h3>${response.results[i].title}</h3><h4>${response.results[i].release_date}</h4><p>${response.results[i].overview}</p></div></div><br />`);
         }
